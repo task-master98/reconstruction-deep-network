5ceb2c69d5c6e4b9b28e81ca2b35d29bb06ca3ce
Ishaan Roy <ishaanroy@Ishaans-MacBook-Air.local>
2023-11-23 12:18:36 -0700

    BugFix: FID distance gives unstable values

--- reconstruction_deep_network/metrics/metrics.py
+++ reconstruction_deep_network/metrics/metrics.py
@@ -6,7 +6,7 @@ from scipy import linalg
 def calculate_fretchet_inception_distance(
     mu1: np.ndarray, sigma1: np.ndarray,
     mu2: np.ndarray, sigma2: np.ndarray,
-    eps: float = 1e-6):
+    eps: float = 1e-5):
 
     mu1 = np.atleast_1d(mu1)
     mu2 = np.atleast_1d(mu2)
@@ -14,25 +14,43 @@ def calculate_fretchet_inception_distance(
     sigma1 = np.atleast_2d(sigma1)
     sigma2 = np.atleast_2d(sigma2)
 
-    diff = mu1 - mu2
+    sigma1 += eps * np.eye(sigma1.shape[0])
+    sigma2 += eps * np.eye(sigma2.shape[0])
 
-    covmean, _ = linalg.sqrtm(sigma1.dot(sigma2), disp=False)
-    if not np.isfinite(covmean).all():
-        offset = np.eye(sigma1.shape[0]) * eps
-        covmean = linalg.sqrtm((sigma1 + offset).dot(sigma2 + offset))
+    ssdiff = np.sum((mu1 - mu2)**2.0)
+
+
+    covmean, _ = linalg.sqrtm(sigma1.dot(sigma2), disp=False)    
     
     if np.iscomplexobj(covmean):
         covmean = covmean.real
     
-    covmean_trace = np.trace(covmean)
-    fid = (diff.dot(diff) + np.trace(sigma1) + np.trace(sigma2)) - 2 * covmean_trace
+    fid = ssdiff + np.trace(sigma1 + sigma2 - 2.0 * covmean)
+    fid = np.round(fid, decimals=5)
     return fid
 
-def calculate_activation_statistics(self, prediction: np.ndarray):
+def calculate_activation_statistics(prediction: np.ndarray):
+    if prediction.ndim > 2:
+        prediction = np.squeeze(prediction, axis=(2, 3))
     mu = np.mean(prediction, axis=0)
     sigma = np.cov(prediction, rowvar=False)
     return mu, sigma
     
+if __name__ == "__main__":
+    ...
+# define two collections of activations
+    act1 = np.random.randn(10, 2048)
+    act2 = np.random.randn(10, 2048)    
+    
+
+    mu1, sigma1 = calculate_activation_statistics(act1)
+    mu2, sigma2 = calculate_activation_statistics(act2)
+
+    fid_diff = calculate_fretchet_inception_distance(mu1, sigma1, mu2, sigma2)
+    fid_same = calculate_fretchet_inception_distance(mu1, sigma1, mu1, sigma1)
+    
+    print(fid_diff)
+    print(fid_same)
 
 
 
